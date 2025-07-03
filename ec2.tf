f89d148dda88160edbf21b546b6230485de07cc4
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-03 19:37:16 +0530

    instance

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,13 @@ resource "aws_instance" "k8" {
     ami = var.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = var.sg_id
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
     
 }
