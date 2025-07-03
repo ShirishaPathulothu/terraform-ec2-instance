21ec1bc70e6d7ca366ec1b77c07a098a172e9dc2
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-03 19:17:35 +0530

    instance

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "k8" {
     name = var.name
-    ami_id = var.ami_id
+    ami = var.ami_id
     instance_type = "t3.micro"
-    vpc_security_group_ids = var.sg_id
+    vpc_security_group_ids = ["var.sg_id"]
     
 }
