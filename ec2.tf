8be1daed39b7a3f60a24dcf385c9b9fd4eb72445
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-03 20:19:58 +0530

    instance

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,5 @@ resource "aws_instance" "k8" {
     ami = var.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = var.sg_id
-    egress = var.egress_rule
     
 }
--- variables.tf
+++ variables.tf
@@ -34,14 +34,10 @@ variable "ami_id" {
 }
 
 variable "egress_rule" {
-    default = [
-        (
-            {
+    default = [({
                  from_port = 0
                  to_port = 0
                  protocol = -1
                  cidr_blocks = ["0.0.0.0/0"]
-            }
-        )
-    ]
+            })]
 }
