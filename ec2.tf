8985c06331091ef0e61497540ae1248a94053298
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-07-04 12:58:58 +0530

    instance

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,34 @@
-resource "aws_instance" "k8" {
+resource "aws_instance" "workstation" {
     ami = var.ami_id
     instance_type = "t3.micro"
-    vpc_security_group_ids = var.sg_id
+    vpc_security_group_ids = [aws_security_group.sg_id.id]
     
+    tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "sg_id" {
+  name        = var.sg_name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "sg_id" {
-    type = list(string)
-    default = ["sg-0f81b4954c6b01728"]
-
-}
-
 variable "public_subnet_id" {
     type = string
     default = "subnet-04b690ffdaa49022a"
@@ -14,31 +8,18 @@ variable "instance_type" {
     default = "t3.micro"
 }
 
-variable "user_data" {
-    type = string
-}
-
-variable "tags" {
-    default = {
-        Name = "workstation"
-    }
-}
-
-variable "name" {
-    type = string
-    default = "workstation"
-}
 
 variable "ami_id" {
     default = "ami-09c813fb71547fc4f"
 }
 
-variable "egress_rule" {
-    default = [({
-                 from_port = 0
-                 to_port = 0
-                 protocol = -1
-                 cidr_blocks = ["0.0.0.0/0"]
-                 ipv6_cidr_blocks = ["::/0"]
-            })]
+variable "sg_name" {
+    default = "allow-all"
+}
+
+variable "tags" {
+    type = map
+    default = {
+        Name = "workstation"
+    }
 }
